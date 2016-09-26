d53cd1bf0d124ab53c5481d9b02484cdbd0ffa40
Gustavo Mora <gus@nubleer.com>
2016-09-25 22:15:14 -0500

    setup local db

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :questionnaire, Questionnaire.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "questionnaire_dev",
+  username: "redes",
+  password: "equipo5",
+  database: "tests_redes",
   hostname: "localhost",
   pool_size: 10
